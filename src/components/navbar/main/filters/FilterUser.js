a3c918baec76c5db2ed34b1fd97f7bc991a7351d
morena <web.iamsrn@gmail.com>
2022-04-18 00:38:41 +0400

    search user result - visual

--- src/components/navbar/main/filters/FilterUser.js
+++ src/components/navbar/main/filters/FilterUser.js
@@ -1,4 +1,78 @@
 import SearchPattern from "./SearchPattern"
+import Separator from "../Separator";
+import SearchUser from "./SearchUser";
+
+/*
+
+
+    private String about;
+    private String login;
+    private Boolean isActive;
+    private Boolean isUser;
+    private Collection<Pair> roles;
+
+ */
+
+let response =[{
+        id:1,
+        uuid:"jyttdhjkl;",
+        createdTimestamp: "2017-01-26",
+        modifiedTimestamp: "2017-01-26",
+        isDeleted:false,
+        isTokenAllowed:true,
+
+        login:"testlogin",
+        about:null,
+        isActive:false,
+        isUser:false,
+
+        scope:{value:2, label:"private"},
+        roles:[{value:1, label:"system"},{value:2, label:"post_modifier"}]
+},
+        {
+                id:1,
+                uuid:"jyttdhjkl;",
+                createdTimestamp: "2017-01-26",
+                modifiedTimestamp: "2017-01-26",
+                isDeleted:false,
+                isTokenAllowed:true,
+
+                login:"testlogin",
+                about:null,
+                isActive:false,
+                isUser:false,
+
+                scope:{value:2, label:"private"},
+                roles:[{value:1, label:"system"},{value:2, label:"post_modifier"}]
+        },
+        {
+                id:1,
+                uuid:"jyttdhjkl;",
+                createdTimestamp: "2017-01-26",
+                modifiedTimestamp: "2017-01-26",
+                isDeleted:false,
+                isTokenAllowed:true,
+
+                login:"testlogin",
+                about:null,
+                isActive:false,
+                isUser:false,
+
+                scope:{value:2, label:"private"},
+                roles:[{value:1, label:"system"},{value:2, label:"post_modifier"}]
+        }]
+
+const ResponseMapper =() =>{
+        return (
+            <div>
+                    <Separator.Separator4/>
+                    <div className={"jumbotron bg-light"}>
+                            <h4 className={"text-center"}>Search result</h4>
+                            {response.map(SearchUser)}
+                    </div>
+            </div>
+        );
+}
 
 const FilterUser = () => {
 
@@ -30,6 +104,8 @@ const FilterUser = () => {
             <hr/>
 
             <SearchPattern.SearchButton/>
+
+            <ResponseMapper/>
         </div>
     );
 }
---
+++ src/components/navbar/main/filters/SearchUser.js
@@ -0,0 +1,58 @@
+import Separator from "../Separator";
+
+const RoleMapper = (props) => {
+  return(
+      props.roles.map((role, index) =>
+          <li key={index}>
+              {role.label} (code {role.value})
+          </li>
+  ));
+}
+//roles:[{value:1, label:"system"},{value:2, label:"post_modifier"}]
+const BarSearchUser = () =>{
+    return (
+        <div className={"float-right"}>
+            <button type="button" className="btn btn-outline-secondary">В бан</button>
+            <button type="button" className="btn btn-outline-dark border-0" disabled> </button>
+            <button type="button" className="btn btn-outline-secondary">Удалить</button>
+        </div>
+    )
+}
+
+const SearchUser = (props) => {
+    return (
+        <div>
+            <Separator.Separator4/>
+
+            <h5>Meta info:</h5>
+            <hr/>
+            <p> <strong>Id:</strong> {props.id}</p>
+            <p> <strong>UUID:</strong> {props.uuid}</p>
+            <p> <strong>Created (registered) at </strong> {props.createdTimestamp}</p>
+            <p> <strong>Last time modified at</strong> {props.modifiedTimestamp}</p>
+            <p> <strong>Is active:</strong> {props.isActive ? "yes": "no"}</p>
+            <p> <strong>Is user:</strong> {props.isUser ? "yes": "no"}</p>
+
+            <Separator.Separator1/>
+
+            <h5>Admin-editable info:</h5>
+            <hr/>
+            <p> <strong>Is deleted:</strong> {props.isDeleted ? "yes": "no"}</p>
+            <p> <strong>Is banned:</strong> {props.isTokenAllowed ? "yes": "no"}</p>
+            <p> <strong>Scope:</strong> {props.scope.value} ({props.scope.label})</p>
+            <p> <strong>Roles:</strong> <ul>{<RoleMapper roles={props.roles}/>}</ul> </p>
+
+            <Separator.Separator1/>
+
+            <h5>User-editable info:</h5>
+            <hr/>
+            <p> <strong>Login:</strong> {props.login}</p>
+            <p><strong>About:</strong> {props.about === null ? 'no data' : props.about}</p>
+
+            <hr/>
+            <BarSearchUser/>
+        </div>
+    );
+}
+
+export default SearchUser;
